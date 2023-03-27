a6e307fe42f3c9db31fcefca013b26eb7b297db8
Tw01c <3576850961@qq.com>
2023-03-27 23:00:05 +0800

    增加buffer的setdata

--- Assets/Box/ComputeShader/Scripts/D2Flocking.cs
+++ Assets/Box/ComputeShader/Scripts/D2Flocking.cs
@@ -12,7 +12,7 @@ namespace ComputeShader
 
     public class D2Flocking : MonoBehaviour
     {
-        [SerializeField]
+        
         public UnityEngine.ComputeShader shader;
 
         public float boidSpeed = 1f;
@@ -21,9 +21,11 @@ namespace ComputeShader
         public int boidsCount;
         public float spawnRadius;
         public Transform target;
-
-
-        public int _bufferLength = 128;
+        public List<Transform> boidsList;//用来存放boids的Transform
+        public List<Vector2> minSpawnPositions;
+        public List<Vector2> maxSpawnPositions;
+        
+        public int bufferLength = 256;
         private int _kernelHandle;
         private ComputeBuffer _boidsBuffer;
         private Boid[] _boidsArray;
@@ -32,7 +34,7 @@ namespace ComputeShader
         public int initCount;
         private int _numOfBoids = 0;
         
-        private List<Transform> _boidsList;//用来存放boids的Transform
+        
         /*private List<Rigidbody2D> _rbs;*/
         private AsyncGPUReadbackRequest _readback;
         private bool count;
@@ -47,7 +49,7 @@ namespace ComputeShader
             /*_groupSizeX = Mathf.CeilToInt(boidsCount / (float)x);*/
             // 塞满每个线程组，免得 Compute Shader 中有线程读不到数据，造成读取数据越界
             /*_numOfBoids = _groupSizeX * (int)x;*/
-            _groupSizeX = 256;
+            _groupSizeX = (int)x;
 
             InitBoids();
             InitShader();
@@ -57,8 +59,8 @@ namespace ComputeShader
         {
             /*_boids = new GameObject[numOfBoids];*/
             /*_boidsArray = new Boid[_numOfBoids];*/
-            _boidsList = new List<Transform>(_bufferLength);
-            _boidsArray = new Boid[_bufferLength];//改成一个固定的极大值的数组
+            boidsList = new List<Transform>(bufferLength);
+            _boidsArray = new Boid[bufferLength];//改成一个固定的极大值的数组
             /*_rbs = new List<Rigidbody2D>(_numOfBoids); //delete*/
             
             //之前的做法
@@ -74,20 +76,21 @@ namespace ComputeShader
 
             for (int i = 0; i < initCount; i++)
             {
-                Vector3 pos = transform.position + new Vector3(Random.Range(-5,5),Random.Range(-5,5),0);/*Random.insideUnitSphere * spawnRadius;*/
+                var j = i % 4;
+                Vector3 pos = transform.position + new Vector3(Random.Range(minSpawnPositions[j].x,maxSpawnPositions[j].x), Random.Range(minSpawnPositions[j].y,maxSpawnPositions[j].y),0);/*Random.insideUnitSphere * spawnRadius;*/
                 pos.z = 0;
                 var obj = Instantiate(boidPrefab, pos, Quaternion.identity);
                 AddMonster(obj.transform,false);//这里因为是遍历，所以不要让每次都更新
             }
             Debug.Log("ok");
 
-            for (int i = 0; i < _boidsList.Count; i++)
+            for (int i = 0; i < boidsList.Count; i++)
             {
-                _boidsArray[i] = new Boid(_boidsList[i].position);
-                _boidsArray[i].direction = _boidsList[i].up;
+                _boidsArray[i] = new Boid(boidsList[i].position);
+                _boidsArray[i].direction = boidsList[i].up;
             }
             Vector3 v = new Vector3(-1000, -1000, -1000);
-            for (int i = _boidsList.Count; i < _boidsArray.Length; i++)
+            for (int i = boidsList.Count; i < _boidsArray.Length; i++)
             {
                 _boidsArray[i] = new Boid(v);
                 _boidsArray[i].direction = Vector3.zero;
@@ -99,7 +102,7 @@ namespace ComputeShader
         {
             // 定义大小，鸟的数量和每个鸟结构的大小，一个 Vector3 就是 3 * sizeof(float)
             // 10000 只鸟，每只占 6 * 4 bytes，总共也就占 0.234mib GPU 显存 
-            _boidsBuffer = new ComputeBuffer(_bufferLength, 6 * sizeof(float));
+            _boidsBuffer = new ComputeBuffer(bufferLength, 6 * sizeof(float));
             _boidsBuffer.SetData(_boidsArray);
             // 设置 buffer 到 Compute Shader，同时设置要调用的计算的函数 Kernel
             shader.SetBuffer(_kernelHandle, "boidsBuffer", _boidsBuffer);
@@ -107,25 +110,41 @@ namespace ComputeShader
             shader.SetVector("flockPosition", target.transform.position);
             shader.SetFloat("neighbourDistance", neighbourDistance);
             shader.SetInt("boidsCount", boidsCount);
+            shader.Dispatch(_kernelHandle, _groupSizeX, 1, 1);
             //第一次请求的初始化,注意这里的buffer大小是buffer的长度
-            _readback = AsyncGPUReadback.Request(_boidsBuffer, _bufferLength * 6 * sizeof(float), 0);
+            _readback = AsyncGPUReadback.Request(_boidsBuffer, bufferLength * 6 * sizeof(float), 0, GetGPUData);
         }
+        
 
         private RaycastHit2D[] _results = new RaycastHit2D[10];
         void Update()
         {
             if (Input.GetMouseButtonDown(0))
             {
-                var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Physics2D.CircleCastNonAlloc(pos, 1, Vector2.up,_results);
-                for (int i = 0; i < _results.Length; i++)
-                {
-                    RemoveMonster(_results[i].transform,false);
-                }
-                UpdateBuffer();
+                RemoveTest();
             }
             /*Request();*/
             Callback();
+        }
+
+        void RemoveTest()
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Physics2D.CircleCastNonAlloc(pos, 1, Vector2.up,_results);
+            if (_results == null)
+                return; 
+            for (int i = 0; i < _results.Length; i++)
+            {
+                var t = _results[i].transform;
+                if (t.gameObject.CompareTag("Monster"))
+                {
+                    RemoveMonster(t,false);
+                }
+                
+                
+                    
+            }
+            UpdateArray();
         }
 
         /*/// <summary>
@@ -207,10 +226,10 @@ namespace ComputeShader
             if (_readback.done)
             {
 
-                for (int i = 0; i < _boidsList.Count; i++)
+                for (int i = 0; i < boidsList.Count; i++)
                 {
                     //用list去承载buffer里的内容
-                    _boidsList[i].position = _boidsArray[i].position;
+                    boidsList[i].position = _boidsArray[i].position;
                     //这句是刚体的移动
                     /*
                      * _rbs[i].MovePosition(_boidsArray[i].position);
@@ -222,51 +241,60 @@ namespace ComputeShader
                     }*/
 
                 }
-                UpdateBuffer();
+                UpdateArray();
                 //如果完成了
                 // 设置鸟的 position 和 rotation
 
                 //清空数据，填入新的请求，当完成时，将数据拷贝到数组中
                 // 设置每一帧会变的变量
+                //注意改变数组之后要重新设置buffer，因为改变了buffer里的数组后，需要设置到gpu中才会改变里面的值
+                shader.SetBuffer(_kernelHandle, "boidsBuffer", _boidsBuffer);
                 shader.SetFloat("deltaTime", Time.deltaTime);
                 shader.SetVector("flockPosition", target.transform.position);
                 // 调用 Compute Shader Kernel 来计算
                 shader.Dispatch(_kernelHandle, _groupSizeX, 1, 1);
-                _readback = AsyncGPUReadback.Request(_boidsBuffer, _bufferLength * 6 * sizeof(float), 0,
-                    (o) => { o.GetData<Boid>().CopyTo(_boidsArray); });
+                _readback = AsyncGPUReadback.Request(_boidsBuffer, bufferLength * 6 * sizeof(float), 0,GetGPUData);
+                
             }
 
         }
+        /// <summary>
+        /// 获取数据的回调函数
+        /// </summary>
+        /// <param name="readback"></param>
+        void GetGPUData(AsyncGPUReadbackRequest readback)
+        {
+            readback.GetData<Boid>().CopyTo(_boidsArray);
+        }
 
-
-        void AddMonster(Transform target,bool needUpdate = true)
+    void AddMonster(Transform target,bool needUpdate = true)
         {
             _numOfBoids++;
-            _boidsList.Add(target);
+            boidsList.Add(target);
             if(needUpdate)
-                UpdateBuffer();
+                UpdateArray();
         }
 
         void RemoveMonster(Transform target,bool needUpdate = true) 
         {
             _numOfBoids--;
-            target.position = 1000 * Vector2.one;
-            _boidsList.Remove(target);
+            target.position = -1000 * Vector2.one;
+            boidsList.Remove(target);
             if(needUpdate)
-                UpdateBuffer();
+                UpdateArray();
         }
 
 
-        void UpdateBuffer()
+        void UpdateArray()
         {
             Vector3 v = new Vector3(-1000, -1000, -1000);
-            for (int i = 0; i < _boidsList.Count; i++)
+            for (int i = 0; i < boidsList.Count; i++)
             {
-                _boidsArray[i].position = _boidsList[i].position;
-                _boidsArray[i].direction = _boidsList[i].up;
+                _boidsArray[i].position = boidsList[i].position;
+                _boidsArray[i].direction = boidsList[i].up;
             }
 
-            for (int i = _boidsList.Count; i < _boidsArray.Length; i++)
+            for (int i = boidsList.Count; i < _boidsArray.Length; i++)
             {
                 _boidsArray[i].position = v;
                 _boidsArray[i].direction = Vector3.zero;
