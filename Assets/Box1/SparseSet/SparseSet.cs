573d25deb3163f1e257e5d19683612f96dfa43cf
Tw01c <3576850961@qq.com>
2023-05-16 23:51:53 +0800

    写了一个稀疏集和测试
    仍然有装箱，需要优化

---
+++ Assets/Box1/SparseSet/SparseSet.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Box1.SparseSet
+{
+    public class Entity
+    {
+        public int id;
+
+        public Entity(int i)
+        {
+            id = i;
+        }
+    }
+    public class SparseSet<T>:ICollection<T> where T: Entity
+    {
+        private int[] _sparse;
+        private T[] _density;
+        private int _count;
+        private int _capacity;
+        private int _maxValue;
+
+        public SparseSet(int capacity,int maxValue)
+        {
+            _capacity = capacity;
+            _maxValue = maxValue;
+            //创建容量和稀疏数组的最大值。
+            _density = new T[capacity];
+            _sparse = new int[maxValue];
+            
+
+        }
+        
+        public void Add(T item)
+        {
+            int id = item.id;
+            if (id > _maxValue)
+            {
+                return;
+            }
+            if (_count > _capacity)
+            {
+                return;
+            }
+
+            _density[_count++] = item;
+            _sparse[id] = _count;
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            var id = item.id;
+            return Contains(id);
+        }
+
+        public bool Contains(int id)
+        {
+            if (id >= _maxValue)
+            {
+                return false;
+            }
+            int densityId = _sparse[id];
+            if (densityId < _count && _density[densityId].id == id)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            int id = item.id;
+            return Remove(id);
+        }
+        public bool Remove(int id)
+        {
+            int densityId = _sparse[id];
+            if (densityId >= _count)
+            {
+                return false;
+            }
+            _count--;
+            _density[densityId] = _density[_count];
+            _sparse[_density[densityId].id] = densityId;
+            return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                yield return _density[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count => _count;
+        public bool IsReadOnly { get; }
+    }
+}
---
+++ Assets/Box1/SparseSet/SparseSetMain.cs
@@ -0,0 +1,174 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Debug = UnityEngine.Debug;
+
+namespace Box1.SparseSet
+{
+    /// <summary>
+    /// 直接在这里写明测试结果
+    /// 100w数据
+    /// |             |稀疏集    |List    |Dic   |
+    /// |遍历         |18.4ms  |18.3ms  |31.4ms|
+    /// |移除9w连续数据|1.8ms   |2574.3ms|5.3ms |
+    /// |清空数据     |0.0367ms|0.556ms |2.57ms|
+    /// 可以看出：
+    /// 1. 遍历上list和稀疏集由于是数组形式遍历，所以速度差不多，dic也不慢毕竟数据有100w。注意，这里基本上在数据超过10w的时候稀疏集遍历速度才会逐渐超过list
+    /// 2. 连续移除数据，list由于每次都会产生位移的原因，所以肯定是很慢的，这里list的移除其实可以优化的，主要比较一下原生性能吧。但是也能看出稀疏集的移除性能是很优秀的。
+    /// 3. 清空数据 由于稀疏集只是进行了计数置为0的操作，所以速度肯定是最快的，其他两个还进行了遍历处理，所以肯定慢。
+    /// 4. 在查询的时候，也是字典的五倍
+    /// 改进思考：由于稀疏集sparse占据的空间会随着数值增大而无限增大，这样浪费了很多空间，而在ECS中，我们进行随机查询的次数并不会达到这样的数量级，所以其实可以将sparse换成类似dic的映射关系的数据结构。或者我觉得直接用dic也可以。
+    /// </summary>
+    public class SparseSetMain : MonoBehaviour
+    {
+        private const int testNums = 100000;
+        private List<Entity> _entities = new List<Entity>(testNums);
+        SparseSet<Entity> ss = new SparseSet<Entity>(testNums, testNums);
+        private Dictionary<int, Entity> dic = new Dictionary<int, Entity>(testNums);
+
+        private Stopwatch _stopwatch;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _stopwatch = new Stopwatch();
+            for (int i = 0; i < testNums; i++)
+            {
+                int key = i;
+                Entity entity = new Entity(key);
+                _entities.Add(entity);
+                ss.Add(entity);
+                dic.Add(key,entity);
+            }
+            Debug.Log("Traverse S:"+TraverseSparseSet(ss)+" List:" +TraverseSList(_entities) + " Dic:" + TraverseDic(dic));
+            Debug.Log("Contains S:"+ContainsSparse(ss,10,100) + " Dic:"+ ContainsDic(dic,10,100));
+            Debug.Log("Remove S:"+RemoveSparseSet(ss,10,1000)+" List:" +RemoveList(_entities,10,1000) +" Dic:" + RemoveDic(dic,10,1000));
+            Debug.Log("Clear S:"+ClearSparseSet(ss)+" List:" +ClearList(_entities) + " Dic:"+ClearDic(dic));
+            Debug.Log(ss.Contains(50000));
+            
+        }
+
+        long TraverseDic(Dictionary<int,Entity> dic)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            int sum = 0;
+            foreach (var e in dic)
+            {
+                sum += e.Value.id;
+            }
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+        long RemoveDic(Dictionary<int,Entity> d,int start,int end)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            for (int i = start; i < end; i++)
+            {
+                dic.Remove(i);
+            }
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+        long ContainsDic(Dictionary<int,Entity> d,int start,int end)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            for (int i = start; i < end; i++)
+            {
+                d.ContainsKey(i);
+            }
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+        long ClearDic(Dictionary<int,Entity> d)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            d.Clear();
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+
+        long TraverseSparseSet(SparseSet<Entity> s)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            int sum = 0;
+            foreach (var v in s)
+            {
+                sum += v.id;
+            }
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+
+        long ContainsSparse(SparseSet<Entity> s,int start,int end)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            for (int i = start; i < end; i++)
+            {
+                s.Contains(i);
+            }
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+
+        long RemoveSparseSet(SparseSet<Entity> s,int start,int end)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            for (int i = start; i < end; i++)
+            {
+                s.Remove(i);
+            }
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+
+        long ClearSparseSet(SparseSet<Entity> s)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            s.Clear();
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+        
+        long TraverseSList(List<Entity> e)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            int sum = 0;
+            foreach (var v in e)
+            {
+                sum += v.id;
+            }
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+        long RemoveList(List<Entity> e,int start,int end)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            for (int i = start; i < end; i++)
+            {
+                e.RemoveAt(i);
+            }
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+        
+        long ClearList(List<Entity> e)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            e.Clear();
+            _stopwatch.Stop();
+            return _stopwatch.ElapsedTicks;
+        }
+        
+    }
+}
