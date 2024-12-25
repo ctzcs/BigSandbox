0ac8a74bfa66309ea8a594668e455b1460c7c3fb
tianwen <yaohii@foxmail.com>
2024-12-25 20:31:54 +0800

    saves

--- Assets/EditorBox/Editor/InspectorComponentEditor.cs
+++ Assets/EditorBox/Editor/InspectorComponentEditor.cs
@@ -1,10 +1,20 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 
 namespace EditorBox.Editor
 {
     [CustomEditor(typeof(InspectorComponent))]
     public class InspectorComponentEditor:UnityEditor.Editor
     {
-        
+        private SerializedProperty _inspectorList;
+        private void OnEnable()
+        {
+            Init();
+        }
+
+        void Init()
+        {
+            _inspectorList = serializedObject.FindProperty("InspectorList");
+        }
     }
 }
